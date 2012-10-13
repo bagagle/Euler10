3916ea9947beb6b0035e80b905d7df3999f6e68e
Michael <bagagle@gmail.com>
2012-10-12 18:20:38 -0700

    Update euler10.py
    Fixed problems. Still runs incredibly slowly.

--- euler10.py
+++ euler10.py
@@ -1,28 +1,26 @@
 '''The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million'''
-
-
 def isprime(myNum):
-    if(myNum < 2):
+    if myNum < 2 :
         return False
     #using 'is' instead of '=='
-    elif(myNum is 2):
+    if myNum == 2 :
         return True
     #must define myNum as int, can't process float
     #Only need to process until .5 of myNum
     #range(start, finish, steps)
-    for x in range(3, int(myNum*0.5)+1, 2): 
+    myMax = myNum**0.5+1
+    for x in range(3, int(myMax), 2):
         if myNum % x == 0:
             return False
     return True
     
 def main():
-    mysum = 0
-    for myNum in range(100):
+    mysum = 2
+    for myNum in range(3, 2000000, 2):
         if(isprime(myNum) is True):
             mysum += myNum
-        else:
-            pass
-    print(mysum) #ATTN: s not printing the correct numbers
+
+    print(mysum)
 
 if __name__ == "__main__":main()
